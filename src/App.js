c559be5fbea991ffb6809bc15ca022ca58386aa8
Noah Dirck <78816489+Niche-Quiche@users.noreply.github.com>
2022-04-04 14:38:38 -0400

    .

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Footer from "./components/Footer";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Modal from "./components/Modal";
-import Resume from "./components/Resume";
+// import Modal from "./components/Modal";
+// import Resume from "./components/Resume";
 
 class App extends Component {
 
